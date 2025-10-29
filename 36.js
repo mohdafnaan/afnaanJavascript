57d0b0a858b2a0203fcae498593230cf54e8ba21
mohdafnaan <mohdafnaan833@gmail.com>
2025-10-29 12:55:07 +0000

    updated object file

--- 36.js
+++ 36.js
@@ -13,3 +13,22 @@ let person=["afnaan", 4754564645,10,true,null,undefined];
 //              0          1      2  3    4      5
 
 // accessing Array value
+// console.log(person[0],person[1]);
+// console.log(person[4]);
+
+// Adding a new value in array
+
+// console.log(person);
+person[6] = "mohammed"
+// console.log(person);
+
+// updating a array 
+// console.log(person);
+// person[2]=20;
+// person[3]=true; 
+// console.log(person);
+
+//deletng a array
+console.log(person);
+delete person[6];
+console.log(person);
